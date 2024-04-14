276d314e36f1d65016c6836625c9c914799f5684
xiaozhang <1016653792@qq.com>
2024-04-14 18:45:17 +0800

    [功能] debian代理切换脚本

--- shell/manager_proxy.sh
+++ shell/manager_proxy.sh
@@ -7,28 +7,31 @@ enable_proxy() {
     read -p "Enter proxy IP: " proxy_ip
     read -p "Enter proxy port: " proxy_port
 
-    # 将代理设置添加到~/.bashrc中
+    # 将代理设置添加到 ~/.bashrc 中
     echo "export http_proxy=\"http://$proxy_ip:$proxy_port\"" >> ~/.bashrc
     echo "export https_proxy=\"https://$proxy_ip:$proxy_port\"" >> ~/.bashrc
     echo "export ftp_proxy=\"ftp://$proxy_ip:$proxy_port\"" >> ~/.bashrc
+    echo "export all_proxy=\"socks5://$proxy_ip:$proxy_port\"" >> ~/.bashrc
 
-    # 重新加载.bashrc文件
+    # 重新加载 ~/.bashrc 文件
     source ~/.bashrc
 
     echo "Proxy enabled"
     echo "HTTP_PROXY=$http_proxy"
     echo "HTTPS_PROXY=$https_proxy"
     echo "FTP_PROXY=$ftp_proxy"
+    echo "SOCKS_PROXY=$all_proxy"
 }
 
 # 函数：禁用代理
 disable_proxy() {
-    # 从~/.bashrc中删除代理设置
+    # 从 ~/.bashrc 中删除代理设置
     sed -i '/http_proxy/d' ~/.bashrc
     sed -i '/https_proxy/d' ~/.bashrc
     sed -i '/ftp_proxy/d' ~/.bashrc
+    sed -i '/all_proxy/d' ~/.bashrc
 
-    # 重新加载.bashrc文件
+    # 重新加载 ~/.bashrc 文件
     source ~/.bashrc
 
     echo "Proxy disabled"
